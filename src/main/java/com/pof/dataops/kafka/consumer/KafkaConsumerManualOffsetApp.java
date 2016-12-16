d73d34bc92ab075c1da5092b7629030cca086b4c
Glory Lo <glo@pof.com>
2016-12-16 15:00:35 -0800

    added example for commitSync

--- src/main/java/com/pof/dataops/kafka/consumer/KafkaConsumerManualOffsetApp.java
+++ src/main/java/com/pof/dataops/kafka/consumer/KafkaConsumerManualOffsetApp.java
@@ -1,5 +1,6 @@
 package com.pof.dataops.kafka.consumer;
 
+import org.apache.kafka.clients.consumer.CommitFailedException;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -19,7 +20,7 @@ public class KafkaConsumerManualOffsetApp {
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("group.id", "firstGroup" );
-        props.put("enable.auto.commit", "true");
+        props.put("enable.auto.commit", "false");
         props.put("max.partition.fetch.bytes", "2097152");
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
@@ -27,19 +28,36 @@ public class KafkaConsumerManualOffsetApp {
         topics.add("test");
         consumer.subscribe(topics);
 
+        final int batchSize = 10;
+
         try {
             while(true) {
 
                 ConsumerRecords<String, String> records = consumer.poll(100);
 
+                int countSize = 0;
+
+
                 for (ConsumerRecord<String, String> record : records) {
+
                     System.out.println(
-                       String.format("Topic: %s, Partition: %d, Offset: %d, Key: %s, Value: %s", record.topic(), record.partition(), record.offset(), record.key(), record.value())
+                            String.format("Topic: %s, Partition: %d, Offset: %d, Key: %s, Value: %s", record.topic(), record.partition(), record.offset(), record.key(), record.value())
                     );
+                    countSize++;
+
+                    if (countSize > batchSize) {
+                        countSize = 0;
+                        System.out.println("Committing changes...");
+                        consumer.commitSync();
+                    }
                 }
 
             }
-        } catch (Throwable e) {
+        }
+        catch(CommitFailedException e) {
+            System.out.println("Unexpected commit exception"  + e.getMessage());
+        }
+        catch (Throwable e) {
             System.out.println(e.getMessage());
         } finally {
             consumer.close();
